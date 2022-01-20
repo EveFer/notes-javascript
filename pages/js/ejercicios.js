d7c8a33e124edd48c3b44e95031c6c1701c4b9e6
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2022-01-19 20:56:07 -0600

    arrays and objects exercises

--- pages/js/ejercicios.js
+++ pages/js/ejercicios.js
@@ -80,18 +80,18 @@ export default function ejercicips () {
                   </div>
                 </div>
               </div>
-              {/* <div className='accordion-item'>
+              <div className='accordion-item'>
                 <h2 className='accordion-header' id='headingTwo'>
                   <button className='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#collapseTwo' aria-expanded='false' aria-controls='collapseTwo'>
-                    Accordion Item #2
+                    Arrays and Objects
                   </button>
                 </h2>
                 <div id='collapseTwo' className='accordion-collapse collapse' aria-labelledby='headingTwo' data-bs-parent='#accordionExample'>
                   <div className='accordion-body'>
-                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    <a href="https://gist.github.com/EveFer/508433e44650d5f14789fcbcbe271a2b" target="_blank" rel="noreferrer">Link a Ejercicios 🤓</a> 
                   </div>
                 </div>
-              </div> */}
+              </div>
               {/* <div className='accordion-item'>
                 <h2 className='accordion-header' id='headingThree'>
                   <button className='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#collapseThree' aria-expanded='false' aria-controls='collapseThree'>
